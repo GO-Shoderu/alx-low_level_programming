749e575f892c4765be4cafece00fe54a3c4e03c7
GO-Shoderu <go.shoderu@gmail.com>
2022-12-09 13:49:25 -0800

    Adding the 0 script as well as the documentation to the readme file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main- this is the entry point of the program
+ *
+ * Return: this returns a default value of 0
+*/
 int main(void)
 {
 	int n;
