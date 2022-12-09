27cd46f743dcd17bfa8cdc64133c23ec76e1e807
GO-Shoderu <go.shoderu@gmail.com>
2022-12-08 18:32:17 -0800

    Adding the 101 script and it documentation in the readme file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - This is the entry level of the project
+ *This entry level is called main
+ *Return: It has a return statement
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
