d0cfd703cfe39dd05c38f08dcdab14501f2f2025
GO-Shoderu <go.shoderu@gmail.com>
2022-12-08 17:08:18 -0800

    Adding the 4 script and it documentation in the readme file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - This is the entry level of the project
+ *This entry level is called main
+ *Return: It has a return statement
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
