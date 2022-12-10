ff7273bf26b9022b060cac135419dab516f475df
GO-Shoderu <go.shoderu@gmail.com>
2022-12-10 06:50:36 -0800

    Adding 100 advance script as well as the documentation for it in the readme

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 48; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for(j = 49; j <= 57; j++)
+		for (j = 49; j <= 57; j++)
 		{
-			if(j > i)
+			if (j > i)
 			{
 				putchar(i);
 				putchar(j);
 
-				if(i < 56 && j <= 57)
+				if (i < 56 && j <= 57)
 				{
 					putchar(44);
 					putchar(32);
