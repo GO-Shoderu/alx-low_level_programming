1a21024c966bc3b9b0ec9ae1efcfd174f8686274
GO-Shoderu <go.shoderu@gmail.com>
2022-12-14 03:59:40 -0800

    Adding the 100-times_table C program along with its documentation on the read me file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,9 @@ void print_times_table(int n)
 	int i;
 	int j;
 	int total;
-	int tens;
 
 	if ((n > 15) || (n < 0))
-	{
 		return;
-	}
 
 	for (i = 0; i <= n; i++)
 	{
@@ -30,23 +27,18 @@ void print_times_table(int n)
 				_putchar((total % 10) + '0');
 			} else if (total >= 100)
 			{
-				tens = total % 100;
 				_putchar((total / 100) + '0');
-				_putchar((tens / 10) + '0');
-				_putchar((tens % 10) + '0');
+				_putchar(((total % 100) / 10) + '0');
+				_putchar(((total % 100) % 10) + '0');
 			} else
 			{
 				if (j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(total + '0');
-				} else
-				{
-					_putchar(total + '0');
 				}
+				_putchar(total + '0');
 			}
-
 			if (j < n)
 			{
 				_putchar(',');
