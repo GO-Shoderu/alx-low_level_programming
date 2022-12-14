ab5da7e872663ead2dbe0605ac01d88e5adb3424
GO-Shoderu <go.shoderu@gmail.com>
2022-12-14 15:03:29 -0800

    Adding the files that prints out the largest of 3 inputed numbers along with it documentation on README.md file

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,8 +1,23 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
 
 int main(void)
 {
-	printf("%d\n", largest_number(972, -98, 0));
-	return (0);
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
 }
