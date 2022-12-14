4c678dc58a9b6902d7cd139f87e9dd12a9d8bc3d
GO-Shoderu <go.shoderu@gmail.com>
2022-12-13 16:44:48 -0800

    Adding 8-24_hours  C program along with its documentation on the read me file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 
 	for (hours = 0; hours <= 23; hours++)
 	{
-		for(mins = 0; mins <= 59; mins++)
+		for (mins = 0; mins <= 59; mins++)
 		{
-			_putchar((hours/10) + '0');
+			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((mins / 10) + '0');
