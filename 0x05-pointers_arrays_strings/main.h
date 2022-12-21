1c833c2f74aa63dedff2528602a67b1a98aea12c
GO-Shoderu <go.shoderu@gmail.com>
2022-12-20 16:37:32 -0800

    Adding reset_to_98 script as well as it documentation on readme

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,21 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - prints input using standard library
- * @c: The character to print
- * Return: 1 is success
- *
- * print_alphabet - prints all the alphabets in lowercase
- *
- * print_alphabet_x10 - prints all the alphabets in lowercase ten times
- *
- * File: main.h
- * Author: Gabriel Shoderu
- * A header file containing function declarations
- * These functions are initialized in the .c file
- */
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
