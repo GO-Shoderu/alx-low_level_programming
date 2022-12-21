77d052d97f5f1fef04676279939ebaf40b2de391
GO-Shoderu <go.shoderu@gmail.com>
2022-12-21 01:49:48 -0800

    Adding the rev_string script with it documentation on the README file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	int t_size;
 	char temp;
 
-	for (iter = _strlen(s), t_size = 0; t_size <= (_strlen(s) / 2);)
+	for (iter = _strlen(s), t_size = 0; t_size <= ((_strlen(s) / 2) - 1);)
 	{
 		temp = *(s + t_size);
 		*(s + t_size) = *(s + (iter - 1));
