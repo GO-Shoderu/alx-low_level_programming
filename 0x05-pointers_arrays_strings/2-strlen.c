cf4649635cfbdc58394908dcd0761e102a73b9e5
GO-Shoderu <go.shoderu@gmail.com>
2022-12-20 17:01:49 -0800

    Adding strlen  script as well as it documentation on readme

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - entry level.
+ * @s: input from user
+ * 
+ * Return: Always integer returned
+ */
+
+int _strlen(char *s)
+{
+	int iter;
+
+	for (iter = 0; s[iter] != '\0'; iter++);
+
+	return (iter);
+}
