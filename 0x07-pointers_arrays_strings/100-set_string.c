426b75ad74151cce613debfcafcf4f4a8546253d
GO-Shoderu <go.shoderu@gmail.com>
2023-01-04 03:21:46 -0800

    Adding the set string script

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - set strings
+ * @s: pointer to another pointer
+ * @to: pointer to a char
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s  = to;
+}
