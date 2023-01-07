2d853599c62e9def47a92a50645aa2d05fa92cb2
GO-Shoderu <go.shoderu@gmail.com>
2023-01-06 16:26:49 -0800

    Adding 5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - entry level.
+ * @n: user input
+ * Return: Always returns the calculated pow
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - helper function
+ * @x: user input
+ * @y: controller
+ * Return: returns the calculated squre root
+ */
+
+int _sqrt(int x, int y)
+{
+	if (x < 0)
+		return (-1);
+
+	if ((y * y) > x)
+		return (-1);
+
+	if ((y * y) == x)
+		return (y);
+
+	return (_sqrt(x, y + 1));
+}
