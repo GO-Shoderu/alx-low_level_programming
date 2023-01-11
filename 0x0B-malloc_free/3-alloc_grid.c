ed2e940f6e81d6f4383423baacf184a100945a4b
GO-Shoderu <go.shoderu@gmail.com>
2023-01-11 14:15:54 -0800

    Adding 3-alloc_grid script as well as it documention on the readme file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,8 +4,8 @@
 /**
  * alloc_grid - this function create 2 dimensional array of int(s).
  * @width: size of width/row
- * @height: size of height/column 
- * 
+ * @height: size of height/column
+ *
  * Return: returns 2 dimensional array of int(s).
  */
 
@@ -24,25 +24,26 @@ int **alloc_grid(int width, int height)
 		free(newArray);
 		return (NULL);
 	}
-	
+
 	for (iterator = 0; iterator < height; iterator++)
 	{
 		newArray[iterator] = (int *)malloc(sizeof(int) * width);
-		
-		if (newArray[iterator] == NULL)
+
+		if (newArray[iterator] != NULL)
+		{
+			for (iterator_ = 0; iterator_ < width; iterator_++)
+				newArray[iterator][iterator_] = 0;
+		} else
 		{
 			while (iterator >= 0)
 			{
 				free(newArray[iterator]);
 				iterator--;
 			}
-			
+
 			free(newArray);
 			return (NULL);
-		} else
-		{
-			for (iterator_ = 0; iterator_ < width; iterator_++)
-				newArray[iterator][iterator_] = 0;
+
 		}
 	}
 
