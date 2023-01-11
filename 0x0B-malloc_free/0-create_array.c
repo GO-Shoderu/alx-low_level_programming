1880b3a27188c13c4baf393c9a68a2b58654c3d9
GO-Shoderu <go.shoderu@gmail.com>
2023-01-11 12:35:31 -0800

    Adding 0-create_array script as well as it documention on the readme file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - this function creates an array.
+ * @size: input from user
+ * @c: input from user
+ *
+ * Return: a char array is returned.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int iterator = 0;
+	char *newArray;
+
+	if (size == 0)
+		return (NULL);
+
+	newArray = malloc(sizeof(char) * size);
+
+	while (iterator < size)
+	{
+		newArray[iterator] = c;
+		iterator++;
+	}
+
+	return (newArray);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+List of programs and there respective responsibility
+
+- 0-create_array.c
+	this is responsible for creating an array based on a size given as well as what the array must contain.
