09e59ac6bd59bd8ec35a308637c8f2ce90fdbbd6
GO-Shoderu <go.shoderu@gmail.com>
2023-01-18 13:14:06 -0800

    Adding 2-int_index script

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,6 +15,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
 	for (iter = 0; iter < size; iter++)
 	{
 		if (cmp(array[iter]))
