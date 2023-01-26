fa0cd3a6525d1a8c95d2d5b2bc1c48493544a5b1
GO-Shoderu <go.shoderu@gmail.com>
2023-01-26 15:55:35 -0800

    Adding 0-print scripts

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list- entry level
+ * @h: the list being passed
+ * Return: size of the passed in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		counter++;
+
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
+
+		h = h->next;
+	}
+
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Exploring the power of linked lists
