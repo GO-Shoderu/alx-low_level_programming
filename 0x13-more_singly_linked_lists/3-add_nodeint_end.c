cc0bd20aa2dd43b96d8e9746e3afdae0d8fe116f
GO-Shoderu <go.shoderu@gmail.com>
2023-01-30 12:42:12 -0800

    Adding more linked list scripts

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,28 +1,35 @@
 #include "lists.h"
 
 /**
- * add_nodeint- entry level
+ * add_nodeint_end- entry level
  * @head: this is the list that needs to be checked
  * @n: initial size of the list
  *
  * Return: size of the list
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t **temp = head;
+	listint_t *temp = *head;
 	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (!newNode)
 		return (NULL);
 
-	while (temp->next)
-		temp = temp->next;
-
 	newNode->n = n;
 	newNode->next = NULL;
 
-	temp->next = newNode;
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
 
-	return (temp->next);
+		temp->next = newNode;
+
+		return(temp->next);
+	} 
+
+	*head = newNode;
+
+	return (newNode);
 }
