55234e47482440c8f94ca849eee660a6c9b6abe7
GO-Shoderu <go.shoderu@gmail.com>
2023-01-31 12:51:54 -0800

    Adding more linked list scripts

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t counter = 0;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		counter++;
+
+		if ((*h - (*h)->next) > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+		}
+		else
+		{
+			*h = NULL;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (counter);
+}
