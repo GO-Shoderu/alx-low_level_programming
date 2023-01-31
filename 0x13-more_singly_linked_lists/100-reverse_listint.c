510bbcc0c74afdc9588968707e93b85047ea8c62
GO-Shoderu <go.shoderu@gmail.com>
2023-01-31 12:28:48 -0800

    Adding more linked list scripts

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,7 +4,6 @@
 /**
  * reverse_listint - reverses the list
  * @head: First node address.
- * 
  * Return: the list is returned
  **/
 
@@ -14,7 +13,7 @@ listint_t *reverse_listint(listint_t **head)
 
 	if (head == NULL || *head == NULL || newNode == NULL)
 		return (NULL);
-	
+
 	while (*head)
 	{
 		*newNode = add_nodeint(newNode, (*head)->n);
