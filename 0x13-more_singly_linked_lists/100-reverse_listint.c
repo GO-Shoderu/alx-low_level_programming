7a039e51c8389aa8522393650168745870b208ed
GO-Shoderu <go.shoderu@gmail.com>
2023-01-31 12:27:48 -0800

    Adding more linked list scripts

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include "2-add_nodeint.c"
+
+/**
+ * reverse_listint - reverses the list
+ * @head: First node address.
+ * 
+ * Return: the list is returned
+ **/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t  **newNode = malloc(sizeof(listint_t));
+
+	if (head == NULL || *head == NULL || newNode == NULL)
+		return (NULL);
+	
+	while (*head)
+	{
+		*newNode = add_nodeint(newNode, (*head)->n);
+		*head = (*head)->next;
+	}
+
+	*head = *newNode;
+	free(newNode);
+
+	return (*head);
+}
