650c8efb3d26e65a28faf1551eb4a86bc69f8868
GO-Shoderu <go.shoderu@gmail.com>
2024-04-04 04:53:12 -0700

    Fixed betty issues

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,25 +4,28 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - this is the entry function
+ * @filename: this gets the name of the file
+ * @letters: this is for retreiving total number of letters
+ *
+ * Return: 0 for errors something else for response
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
 	ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
-
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
+
+	if (filename == NULL || fd == -1)
 		return (0);
-	}
 
 	buffer = (char *)malloc((letters + 1) * sizeof(char));
-	if (buffer == NULL)
+
+	if (!(buffer))
 	{
 		close(fd);
 		return (0);
@@ -37,8 +40,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	buffer[bytes_read] = '\0';
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(fd);
